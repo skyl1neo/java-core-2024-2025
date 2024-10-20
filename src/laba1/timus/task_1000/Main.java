ff54da6d50afd8843ee7843e2c6428bb1065c3fd
skyl1neo <artem.biro1051@gmail.com>
2024-10-20 18:48:48 +0500

    init

--- src/laba1/timus/task_1000/Main.java
+++ src/laba1/timus/task_1000/Main.java
@@ -1,4 +1,19 @@
 package laba1.timus.task_1000;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter printWriter = new PrintWriter(System.out);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int result = a + b;
+
+        printWriter.println(result);
+        printWriter.flush();
+    }
 }
--- src/laba1/timus/task_1293/Main.java
+++ src/laba1/timus/task_1293/Main.java
@@ -1,4 +1,20 @@
 package laba1.timus.task_1293;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter printWriter = new PrintWriter(System.out);
+
+        int N = scanner.nextInt();
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int result = (N * (A*B)) * 2;
+
+        printWriter.println(result);
+        printWriter.flush();
+    }
 }
